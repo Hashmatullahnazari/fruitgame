09d86897bbbe27928d56c7d23964bd6527ed03b9
hashmatullah nazari <hn0153@gmail.com>
2023-01-16 14:47:38 -0600

    added tolowercase so the answer display in upper or lower case

--- programming.js
+++ programming.js
@@ -117,42 +117,71 @@
 // }
 
 
+//use
+// var words = [{word: "apple", hint: "A common red or green fruit"}, {word: "banana", hint: "A long curved fruit with yellow skin when ripe"}, {word: "cherry", hint: "A small round fruit with a smooth red or black skin"},{word: "orange", hint: " fruit with a bright orange skin and a tart-sweet flavor"}];
+// var wordHint = words[Math.floor(Math.random() * words.length)];
+// document.getElementById("word").innerHTML = wordHint.hint;
+// var word = wordHint.word;
+// var currentIndex = 0;
 
-var words = [{word: "apple", hint: "A common red or green fruit"}, {word: "banana", hint: "A long curved fruit with yellow skin when ripe"}, {word: "cherry", hint: "A small round fruit with a smooth red or black skin"},{word: "orange", hint: " fruit with a bright orange skin and a tart-sweet flavor"}];
+// function check() {
+//   var input = document.getElementById("input").value;
+//   if (input == word) {
+//     document.getElementById("result").innerHTML = "Correct!";
+//     currentIndex++;
+//     if (currentIndex === words.length) {
+//       document.getElementById("result").innerHTML = "You have completed the game";
+//       currentIndex = 0;
+      
+//       wordHint = words[Math.floor(Math.random() * words.length)];
+//       word = wordHint.word;
+//       document.getElementById("word").innerHTML = wordHint.hint;
+//        setTimeout(function(){ document.getElementById("result").innerHTML = ""; }, 1000);
+//       document.getElementById("input").value = "";
+//       return
+//     }
+//     wordHint = words[currentIndex];
+//     word = wordHint.word;
+//     document.getElementById("word").innerHTML = wordHint.hint;
+//     document.getElementById("input").value = "";
+//   } else {
+//     document.getElementById("result").innerHTML = "Incorrect.";
+//   }
+//   setTimeout(function(){ document.getElementById("result").innerHTML = ""; }, 1000);
+// }
+
+
+
+
+var words = [{word: "apple", hint: "A common red or green fruit"}, {word: "banana", hint: "A long curved fruit with yellow skin when ripe"}, {word: "cherry", hint: "A small round fruit with a smooth red or black skin"}];
 var wordHint = words[Math.floor(Math.random() * words.length)];
 document.getElementById("word").innerHTML = wordHint.hint;
-var word = wordHint.word;
+var word = wordHint.word.toLowerCase();
 var currentIndex = 0;
 
 function check() {
-  var input = document.getElementById("input").value;
-  if (input == word) {
+  var input = document.getElementById("input").value.toLowerCase();
+  if (input === word) {
     document.getElementById("result").innerHTML = "Correct!";
     currentIndex++;
     if (currentIndex === words.length) {
       document.getElementById("result").innerHTML = "You have completed the game";
       currentIndex = 0;
-      
       wordHint = words[Math.floor(Math.random() * words.length)];
-      word = wordHint.word;
+      word = wordHint.word.toLowerCase();
       document.getElementById("word").innerHTML = wordHint.hint;
-       setTimeout(function(){ document.getElementById("result").innerHTML = ""; }, 1000);
       document.getElementById("input").value = "";
       return
     }
     wordHint = words[currentIndex];
-    word = wordHint.word;
+    word = wordHint.word.toLowerCase();
     document.getElementById("word").innerHTML = wordHint.hint;
     document.getElementById("input").value = "";
   } else {
     document.getElementById("result").innerHTML = "Incorrect.";
   }
-  setTimeout(function(){ document.getElementById("result").innerHTML = ""; }, 1000);
+  setTimeout(function(){ document.getElementById("result").innerHTML = ""; }, 4000);
 }
-
-
-
-
 
 
 
